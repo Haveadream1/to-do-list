720487691df565c7946fff1a9e7ce76bb88c280c
Haveadream1 <260602david@gmail.com>
2023-06-14 19:24:05 +0200

    Layout and create todo

--- src/home.js
+++ src/home.js
@@ -1,5 +1,24 @@
 const home = () => {
-    console.log('girg');
+    let container = document.querySelector('.container');
+    let buttonAdd = document.querySelector('.add');
+
+    const createTodo = () => {
+        let createSection = document.createElement('section');
+        createSection.classList.add('todo');
+        container.appendChild(createSection);
+
+        let createCheckBox = document.createElement('input');
+        createCheckBox.type = 'checkbox';
+        createCheckBox.classList.add('checkbox');
+        createSection.appendChild(createCheckBox);
+
+        let createText = document.createElement('input');
+        createText.type = 'text';
+        createText.classList.add('text');
+        createSection.appendChild(createText);
+     }
+    buttonAdd.addEventListener('click', createTodo);
+
 }
 
 export default home;
