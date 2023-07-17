cee69309c8051cf2b97dd7edfd9567d919e4cd2c
Haveadream1 <260602david@gmail.com>
2023-07-17 10:16:35 +0200

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,36 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    library: "date_fns",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    library: 'date_fns',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.(png|svg|jpg|jpeg|gif|js|mjs|cjs)$/i,
+        exclude: {
+          and: [/node_modules/], // Exclude libraries in node_modules ...
+          not: [
+            // Except for a few of them that needs to be transpiled because they use modern syntax
+            /unfetch/,
+            /d3-array|d3-scale/,
+            /@hapi[\\/]joi-date/,
+          ]
+        },
+        type: 'asset/resource',
+        // test: /\.(?:js|mjs|cjs)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }]
+            ]
+          }
+        },
       },
     ],
   },
