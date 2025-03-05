551f722f4281e075dd1fbf0bf186c2e3afb0b7c9
Haveadream1 <260602david@gmail.com>
2025-03-05 21:36:03 +0100

    feat: add exported functions

--- src/dom_handler.js
+++ src/dom_handler.js
@@ -1,20 +1,56 @@
 // Handle all the DOM with a module
 
 export const renderAside = () => {
-    const aside = document.querySelector('aside');
-    const asideButton = document.querySelector('.aside-button');
+  const aside = document.querySelector('aside');
+  const asideButton = document.querySelector('.aside-button');
 
-    asideButton.addEventListener('click', () => {
-        if (aside.style.visibility === 'hidden') {
-          aside.style.visibility = 'visible';
-          asideButton.style.justifySelf = 'end';
-        } else {
-          aside.style.visibility = 'hidden';
-          asideButton.style.justifySelf = 'start';
-        }
-    });
+  asideButton.addEventListener('click', () => {
+      if (aside.style.visibility === 'hidden') {
+        aside.style.visibility = 'visible';
+        asideButton.style.justifySelf = 'end';
+      } else {
+        aside.style.visibility = 'hidden';
+        asideButton.style.justifySelf = 'start';
+      }
+  });
 };
 
-export const renderMain = () => { 
+export const showError = (input, message) => { 
+  const fieldset = input.parentElement;
+  input.classList.add('error');
+  input.classList.remove('success');
 
+  const small = fieldset.querySelector('small');
+  small.style.padding = '10px 10px 0 10px';
+  small.textContent = message;
 };
+
+export const showSuccess = (input) => {
+  const fieldset = input.parentElement;
+  input.classList.add('success');
+  input.classList.remove('error');
+
+  const small = fieldset.querySelector('small');
+  small.style.padding = '0';
+  small.textContent = '';
+};
+
+export const displayProjectName = () => {
+  const projectButton = document.createElement('button');
+  const projectName = document.querySelector('#project-name').value;
+  const projectSection = document.querySelector('.project-section');
+
+  projectButton.classList.add('project-button');
+  projectButton.textContent = projectName;
+  projectSection.appendChild(projectButton);
+
+  projectButton.addEventListener('click', () => {
+    const selected = document.querySelectorAll('.selected');
+    if (projectButton.classList !== 'selected') { /* CHANGE TO ! WITH CONTAINS */
+      selected.forEach(e => e.classList.remove('selected'));
+      projectButton.classList.add('selected');
+      // window.alert('test');
+    }
+  })
+}
+
