3d43ca1d6c7cdd6e922063873b499398a65a5fb7
Haveadream1 <260602david@gmail.com>
2025-03-06 08:12:29 +0100

    feat: add new functions to export

--- src/dom_handler.js
+++ src/dom_handler.js
@@ -98,6 +98,8 @@ export const createProjectForm = () => {
   })
   projectForm.appendChild(formButton);
   asideFormSection.appendChild(projectForm)
+
+  return projectForm;
 }
 
 export const handleAsideGrid = (state) => {
@@ -113,3 +115,119 @@ export const handleAsideGrid = (state) => {
   }
 }
 
+export const createTodo = (todoName, displayedDate) => {
+  const todo = document.createElement('section');
+  todo.classList.add('todo');
+  todo.classList.add('not-checked');
+
+  const checkbox = document.createElement('input');
+  checkbox.classList.add('checkbox');
+  checkbox.setAttribute('type', 'checkbox');
+  todo.appendChild(checkbox);
+
+  checkbox.addEventListener('change', () => {
+    if (todo.classList.contains('not-checked')) {
+      todo.classList.add('checked');
+      todo.classList.remove('not-checked');
+    } else {
+      todo.classList.add('not-checked');
+      todo.classList.remove('checked');
+    }
+  })
+
+  const name = document.createElement('p');
+  name.classList.add('name');
+  name.textContent = todoName;
+  todo.appendChild(name);
+
+  const date = document.createElement('p');
+  date.classList.add('date');
+  date.textContent = displayedDate;
+  todo.appendChild(date);
+
+  return todo;
+}
+
+export const createTodoForm = () => {
+  const main = document.querySelector('main');
+  const mainFormSection = main.querySelector('.form-section');
+  const addTodoButton = document.querySelector('.add-todo-button');
+
+  const todoForm = document.createElement('form');
+  todoForm.setAttribute('id','todo-form');
+  todoForm.setAttribute('action','post');
+  todoForm.setAttribute('novalidate','true');
+
+  const mainfieldset = document.createElement('fieldset');
+
+  const textFieldset = document.createElement('fieldset');
+  const formText = document.createElement('input');
+  formText.type = 'text';
+  formText.setAttribute('id','todo-name');
+  formText.placeholder = 'Ex: morning task';
+  textFieldset.appendChild(formText);
+
+  const formTextSmall = document.createElement('small');
+  textFieldset.appendChild(formTextSmall);
+  mainfieldset.appendChild(textFieldset);
+
+  const dateFieldset = document.createElement('fieldset');
+  const formDate = document.createElement('input');
+  formDate.type = 'date';
+  formDate.setAttribute('id','todo-date');
+  dateFieldset.appendChild(formDate);
+
+  const formDateSmall = document.createElement('small');
+  dateFieldset.appendChild(formDateSmall);
+  mainfieldset.appendChild(dateFieldset);
+
+  todoForm.appendChild(mainfieldset);
+
+  const formButton = document.createElement('section')
+  formButton.classList.add('form-button');
+
+  const submitProjectButton = document.createElement('button');
+  submitProjectButton.classList.add('submit-project-button');
+  submitProjectButton.setAttribute('type', 'submit');
+  submitProjectButton.textContent = 'Submit';
+  formButton.appendChild(submitProjectButton);
+
+  const cancelProjectButton = document.createElement('button');
+  cancelProjectButton.classList.add('cancel-project-button');
+  cancelProjectButton.setAttribute('type', 'button');
+  cancelProjectButton.textContent = 'Cancel';
+  formButton.appendChild(cancelProjectButton);
+
+  cancelProjectButton.addEventListener('click', () => {
+    todoForm.remove();
+    addTodoButton.disabled = false;
+  })
+
+  todoForm.appendChild(formButton);
+  mainFormSection.appendChild(todoForm);
+
+  return todoForm;
+}
+
+export const handleTabButtons = (state) => {
+  const completedButton = document.querySelector('.completed-button');
+  const todoButton = document.querySelector('.todo-button');
+  const addTodoButton = document.querySelector('.add-todo-button');
+  const notCheckedTodo = document.querySelectorAll('.not-checked');
+
+  if (state === 'completed') {
+    addTodoButton.classList.add('hide');
+
+    completedButton.style.color = 'black';
+    todoButton.style.color = 'var(--light-grey)';
+  } else {
+    addTodoButton.disabled = false;
+    addTodoButton.classList.remove('hide');
+
+    completedButton.style.color = 'var(--light-grey)';
+    todoButton.style.color = 'black';
+  }
+  notCheckedTodo.forEach((e) => {
+    e.classList.remove('hide');
+  });
+}
