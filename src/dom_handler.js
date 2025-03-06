b63d36319e8a6355b3336123d537f6147aa18196
Haveadream1 <260602david@gmail.com>
2025-03-06 07:40:09 +0100

    feat: add new exported functions

--- src/dom_handler.js
+++ src/dom_handler.js
@@ -46,11 +46,70 @@ export const displayProjectName = () => {
 
   projectButton.addEventListener('click', () => {
     const selected = document.querySelectorAll('.selected');
-    if (projectButton.classList !== 'selected') { /* CHANGE TO ! WITH CONTAINS */
+    if (!projectButton.classList.contains('selected')) {
       selected.forEach(e => e.classList.remove('selected'));
       projectButton.classList.add('selected');
       // window.alert('test');
     }
   })
+}
+
+export const createProjectForm = () => {
+  const aside = document.querySelector('aside');
+  const asideFormSection = aside.querySelector('.form-section');
+  const addProjectButton = document.querySelector('.add-project-button');
+
+  const projectForm = document.createElement('form');
+  projectForm.setAttribute('id','project-form');
+  projectForm.setAttribute('action','post');
+  projectForm.setAttribute('novalidate','true');
+
+  const fieldset = document.createElement('fieldset');
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.setAttribute('id','project-name');
+  input.placeholder = 'Ex: Birthday';
+  fieldset.appendChild(input);
+
+  const small = document.createElement('small');
+  fieldset.appendChild(small);
+  projectForm.appendChild(fieldset);
+
+  const formButton = document.createElement('section');
+  formButton.classList.add('form-button');
+  
+  const submitProjectButton = document.createElement('button');
+  submitProjectButton.classList.add('submit-project-button');
+  submitProjectButton.setAttribute('type', 'submit');
+  submitProjectButton.textContent = 'Submit';
+  formButton.appendChild(submitProjectButton);
+
+  const cancelProjectButton = document.createElement('button');
+  cancelProjectButton.classList.add('cancel-project-button');
+  cancelProjectButton.setAttribute('type', 'button');
+  cancelProjectButton.textContent = 'Cancel';
+  formButton.appendChild(cancelProjectButton);
+
+  cancelProjectButton.addEventListener('click', () => {
+    projectForm.remove();
+    aside.style.gridTemplateRows = '100px 80px 1fr';
+    addProjectButton.disabled = false;
+  })
+  projectForm.appendChild(formButton);
+  asideFormSection.appendChild(projectForm)
+}
+
+export const handleAsideGrid = (state) => {
+  const aside = document.querySelector('aside');
+  const addProjectButton = document.querySelector('.add-project-button');
+
+  if (state === 'visible') {
+    aside.style.gridTemplateRows = '100px 200px 1fr';
+    addProjectButton.disabled = true;
+  } else {
+    aside.style.gridTemplateRows = '100px 80px 1fr';
+    addProjectButton.disabled = false;
+  }
 }
 
