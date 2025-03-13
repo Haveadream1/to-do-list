1b1ebd2b85a339e63bb0e8fbf879afa37bfbef19
Haveadream1 <260602david@gmail.com>
2025-03-13 11:10:53 +0100

    refactor: enable clean on loader

--- src/dom_handler.js
+++ src/dom_handler.js
@@ -1,7 +1,11 @@
 // Handle all the DOM with a module
-import deleteIcon from '../dist/images/DeleteIcon.svg'; // Import images this way not with the path in src
-import downIcon from '../dist/images/DownIcon.svg';
-import upIcon from '../dist/images/UpIcon.svg';
+// import deleteIcon from '../dist/images/DeleteIcon.svg'; // Import images this way not with the path in src
+// import downIcon from '../dist/images/DownIcon.svg';
+// import upIcon from '../dist/images/UpIcon.svg';
+
+import deleteIcon from '../dist/e68eb5ceab0af37c15f3.svg'; // Import images this way not with the path in src
+import downIcon from '../dist/a13d1cbbf820048f569d.svg';
+import upIcon from '../dist/0139b722fd2ff1eca652.svg';
 
 export const renderAside = () => {
   const aside = document.querySelector('aside');
